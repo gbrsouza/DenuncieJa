79dda47aea558a4ef1141023033b5d5b6eca43d1
gabriel <gabriel_feg@hotmail.com>
2020-11-30 13:30:06 -0300

    create index page and login page

--- src/main/java/com/ufrn/ihc/denuncieja/controller/IndexController.java
+++ src/main/java/com/ufrn/ihc/denuncieja/controller/IndexController.java
@@ -5,8 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -17,5 +15,15 @@ public class IndexController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/cadastro")
+    public String singUp(){
+        return "signUpInst";
     }
 }
